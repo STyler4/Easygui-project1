f70131678f6fdf36c55009b880b775331e2323ae
STyler4 <98915469+STyler4@users.noreply.github.com>
2023-03-08 10:11:22 +1300

    Exercises

---
+++ Spelling_Convertor_v2.py
@@ -0,0 +1,9 @@
+# Convert US spelling to
+import easygui
+
+nz_word = easygui.enterbox("Please enter the NZ word", "Word to check")
+letters = list(nz_word)
+letters.remove("u")
+easygui.msgbox(f"The American spelling of {nz_word} is {''.join(letters)}",
+               f"Results")
+
