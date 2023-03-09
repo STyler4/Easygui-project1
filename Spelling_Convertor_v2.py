769702c1f70814825f8003441e2f7d66b8e6f4cc
STyler4 <98915469+STyler4@users.noreply.github.com>
2023-03-09 15:08:10 +1300

    Update Spelling_Convertor_v2.py

--- Spelling_Convertor_v2.py
+++ Spelling_Convertor_v2.py
@@ -3,9 +3,22 @@ This program progresses from Spelling_convertor_v1
 This program will replace ise to ize and yse to yze"""
 import easygui
 
-nz_word = easygui.enterbox("Please enter the NZ word", "Word to check")
-letters = list(nz_word)
-letters.remove("u")
-easygui.msgbox(f"The American spelling of {nz_word} is {''.join(letters)}",
-               f"Results")
+def convert_spelling(word):
+    if "our" in word:
+        word = word.replace("our", "or")
+        message = f"US spelling: {word}"
+    elif "ise" in word or "yse" in word:
+        word = word.replace("ise", "ize").replace("yse", "yze")
+        message = f"US spelling: {word}"
+    else:
+        message = "No change in spelling"
+    return message
+
+while True:
+    user_input = easygui.enterbox("Enter a word (or 'exit' to quit): ")
+    if user_input is None or user_input.lower() == "exit":
+        break
+    message = convert_spelling(user_input)
+    easygui.msgbox(message, title="Spelling Converter Result", ok_button="OK", image=None)
+
 
