1877aa6593492c22dda89a3752947fe3f8ab1443
STyler4 <98915469+STyler4@users.noreply.github.com>
2023-03-22 10:03:58 +1300

    Dictionaries

---
+++ Superheroes_v1.py
@@ -0,0 +1,6 @@
+hero_0 = {
+    "name": "Materdon",
+    "power": "flight",
+    "strength": 5
+}
+print(hero_0["name"])
---
+++ Superheroes_v3.py
@@ -0,0 +1,16 @@
+hero_0 = {
+    "name": "Materdon",
+    "power": "flight",
+    "strength": 5
+}
+if "name" in hero_0:
+    print(f"The superhero's name is: {hero_0['name']}")
+else:
+    print("There is no superhero name.")
+
+if "speed" in hero_0:
+    print(f"The superhero's speed is: {hero_0['speed']}")
+else:
+    print(f"There is no speed")
+
+
